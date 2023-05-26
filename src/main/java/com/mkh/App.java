65eda2df0401b18832f97096a05279d10da31d47
mehrshad-kh <mjii@protonmail.com>
2023-05-26 22:21:13 +0330

    Modify App.main()

--- src/main/java/com/mkh/App.java
+++ src/main/java/com/mkh/App.java
@@ -1,6 +1,7 @@
 package com.mkh;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        TwitterServer.main(args);
     }
 }
